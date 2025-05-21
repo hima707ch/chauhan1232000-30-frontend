9e154c4af692671d0c2f30e2be00d9735c6925e3
root <root@8b8bb5a286e8>
2025-05-21 19:56:42 +0000

    Frontend setup and components update

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section bg-gray-200 p-10 text-center'>
+      <h1 className='text-5xl font-bold'>Welcome to Our Real Estate Platform</h1>
+      <p className='text-xl mt-4'>Find your perfect property with us</p>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/HomePage/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from './HeroSection';
+
+const HomePageBody = () => {
+  return (
+    <div>
+      <HeroSection />
+      {/* Other components like a list of properties can go here */}
+    </div>
+  );
+};
+
+export default HomePageBody;
